3906031dc280ecc55032aa285934c2e0b3f411de
sutra-rudi <rudi@sutra.hr>
2024-04-27 12:22:35 +0200

    deploy links

--- src/app/components/AppHeader.tsx
+++ src/app/components/AppHeader.tsx
@@ -15,6 +15,7 @@ import teleIcon from '../img/icons/MOBILE-MENU-SOCIAL-3.svg';
 
 import mobilePapir from '../img/globals/MOBILE-PAPIR.svg';
 import { useAppContext } from '../contexts/store';
+import { useSearchParams } from 'next/navigation';
 
 const AppHeader = () => {
   const {
@@ -22,10 +23,15 @@ const AppHeader = () => {
   } = useAppContext();
 
   const parseByLang = (hrString: string, enString: string) => (userLang === 'hr' ? hrString : enString);
+  const paramsControler = useSearchParams();
+  const checkParams = paramsControler.get('lang');
 
   const navLinksOne = [
     { text: parseByLang('O nama', 'About us'), href: parseByLang('/o-nama', '/about-us') },
-    { text: parseByLang('Smještaj', 'Accommodation'), href: parseByLang('/smjestaj', '/accommodation') },
+    {
+      text: parseByLang('Smještaj', 'Accommodation'),
+      href: parseByLang(`/smjestaj/?lang=${checkParams}`, `/accommodation/?lang=${checkParams}`),
+    },
     { text: parseByLang('Kontakt', 'Contact'), href: '/kontakt' },
     { text: parseByLang('Što posjetiti?', 'What to visit?'), href: parseByLang('/sto-posjetiti', '/what-to-visit') },
   ];
